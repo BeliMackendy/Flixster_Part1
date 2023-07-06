6853edf445b278aa6704ccb06c6132de5e0cf655
Belizaire Mackendy <belizaire.mackendy@gmail.com>
2023-07-05 20:05:16 -0400

    Binding the Adapter to the RecyclerView and Update item_movie

--- app/src/main/java/com/myapplication/flixster/MainActivity.java
+++ app/src/main/java/com/myapplication/flixster/MainActivity.java
@@ -1,17 +1,21 @@
 package com.myapplication.flixster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.myapplication.flixster.adapters.MovieAdapter;
 import com.myapplication.flixster.models.Movie;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.Headers;
@@ -27,15 +31,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Lookup the recyclerview in activity layout
+        RecyclerView rvMovie = findViewById(R.id.rvMovie);
+
+        // Initialize contacts
+        movies = new ArrayList<>();
+        // Create adapter passing in the sample user data
+        MovieAdapter adapter = new MovieAdapter(movies);
+        // Attach the adapter to the recyclerview to populate items
+        rvMovie.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvMovie.setLayoutManager(new LinearLayoutManager(this));
+
+
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(NOW_PLAYING, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 try {
                     JSONArray results = json.jsonObject.getJSONArray("results");
-                    movies = Movie.fromJsonArray(results);
-                    Log.i(TAG, "Results: "+results.toString());
-                    Log.i(TAG, "Movies Size: "+movies.size());
+                    movies.addAll(Movie.fromJsonArray(results));
+                    adapter.notifyDataSetChanged();
+                    Log.i(TAG, "Results: " + results.toString());
+                    Log.i(TAG, "Movies Size: " + movies.size());
                 } catch (JSONException e) {
                     throw new RuntimeException(e);
                 }
